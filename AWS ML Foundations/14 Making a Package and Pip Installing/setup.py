d3abbe006d266286dbe5ac83b7051352ff1281bf
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2020-07-03 08:21:45 +0800

    Create setup.py

---
+++ AWS ML Foundations/14 Making a Package and Pip Installing/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Graph_Distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
