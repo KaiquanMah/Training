f2a9b2700e01b7831fc7fedfc338b6618c0143ef
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2020-07-04 15:28:15 +0800

    add the '.' dot before the module name to import

--- AWS ML Foundations/14 Making a Package and Pip Installing/distributions/Gaussiandistribution.py
+++ AWS ML Foundations/14 Making a Package and Pip Installing/distributions/Gaussiandistribution.py
@@ -1,6 +1,6 @@
 import math
 import matplotlib.pyplot as plt
-from Generaldistribution import Distribution
+from .Generaldistribution import Distribution
 
 class Gaussian(Distribution):
 	""" Gaussian distribution class for calculating and 
