dd806d6ca6b1558dc4b413897112b67d649503c6
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2025-10-30 22:45:02 +0800

    NOTES

---
+++ SQL/Snowflake/Intro GenAI in Snowflake/Intro GenAI in Snowflake.sql
@@ -0,0 +1,23 @@
+How accurate are Snowflake Cortex AI’s translation and sentiment analysis functions?
+Cortex AI leverages large language models (LLMs) for translation and sentiment analysis, providing high accuracy. 
+However, results may vary based on
+- CONTEXT,
+- LANGUAGE complexity, and
+- INDUSTRY-specific jargon.
+It’s recommended to validate outputs for critical business applications.
+
+
+  
+Can I integrate Snowflake Cortex AI with other AI services or external tools?
+Yes! You can combine Cortex AI functions with
+- Snowpark to build advanced ML workflows or 
+- integrate it into Tableau or PowerBI to VIZ insights
+
+  
+What are some best practices for using Snowflake Cortex AI efficiently?
+- Optimize queries by SELECTING ONLY NECESSARY text FIELDS.
+- PREPROCESS text data to REMOVE NOISE before applying AI functions.
+- Use CACHING and INDEXING for frequently accessed text summaries.
+- MONITOR query COSTS to avoid unnecessary compute usage.
+
+
