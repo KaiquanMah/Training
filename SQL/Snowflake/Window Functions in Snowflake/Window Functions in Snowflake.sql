98b6acf17dd4994a8e3e170794a2035e52815389
KaiquanMah <7814298+KaiquanMah@users.noreply.github.com>
2025-10-30 21:14:29 +0800

    NOTES

--- SQL/Snowflake/Window Functions in Snowflake/Window Functions in Snowflake.sql
+++ SQL/Snowflake/Window Functions in Snowflake/Window Functions in Snowflake.sql
@@ -688,6 +688,372 @@ User_833	Station_51	2024-02-04 16:00:00	Evening	9	62.634	8.641
 
 
 
+-- SUM COUNT AVG (aggregations) in window functions - no need ORDER BY
+SELECT
+    user_id,
+    vehicle_model,
+    energy_consumed,
+    charging_duration,
+	
+    -- Count the total number of charging sessions by vehicle_model
+    COUNT(*) OVER(
+        PARTITION BY vehicle_model
+    ) AS total_number_of_charges
+    
+FROM ELECTRIC_VEHICLES.charging
+ORDER BY vehicle_model;
+
+
+USER_ID	VEHICLE_MODEL	ENERGY_CONSUMED	CHARGING_DURATION	TOTAL_NUMBER_OF_CHARGES
+User_507	BMW i3	47.013	1.7	258
+User_831	BMW i3	54.913	2.0	258
+
+
+
+
+
+
+SELECT
+    user_id,
+    vehicle_model,
+    energy_consumed,
+    charging_duration,
+	
+    -- Find the total charging duration, partitioned by vehicle model
+    SUM(charging_duration) OVER(
+        PARTITION BY vehicle_model
+    ) AS total_charging_duration
+
+FROM ELECTRIC_VEHICLES.charging
+ORDER BY vehicle_model;
+
+USER_ID	VEHICLE_MODEL	ENERGY_CONSUMED	CHARGING_DURATION	TOTAL_CHARGING_DURATION
+User_507	BMW i3	47.013	1.7	564.5
+User_831	BMW i3	54.913	2.0	564.5
+
+
+
+
+
+
+SELECT
+    user_id,
+    vehicle_model,
+    energy_consumed,
+    charging_duration,
+	
+    -- Determine the average energy consumed for each vehicle model
+    AVG(energy_consumed) OVER(
+        PARTITION BY vehicle_model
+    ) AS average_energy_consumed
+
+FROM ELECTRIC_VEHICLES.charging
+ORDER BY vehicle_model;
+
+USER_ID	VEHICLE_MODEL	ENERGY_CONSUMED	CHARGING_DURATION	AVERAGE_ENERGY_CONSUMED
+User_507	BMW i3	47.013	1.7	42.450016
+User_831	BMW i3	54.913	2.0	42.450016
+
+
+
+
+
+
+
+SELECT
+    charging_station_location,
+    TO_DATE(charging_start_time) AS charging_date,
+    charging_duration,
+    charging_cost,
+	
+    -- Find the proportion of total charging duration charging location
+    charging_duration / SUM(charging_duration) OVER(
+        PARTITION BY charging_station_location
+    ) AS proportion_of_daily_charging_duration,
+	
+    -- Determine the difference between each session's charging 
+    -- cost and the average charging cost for each charging station location
+    charging_cost - AVG(charging_cost) OVER (
+        PARTITION BY charging_station_location
+    ) AS cost_vs_avg
+
+FROM ELECTRIC_VEHICLES.charging
+
+-- Order the results by charging station location and charging date
+ORDER BY charging_station_location, charging_date;
+
+CHARGING_STATION_LOCATION	CHARGING_DATE	CHARGING_DURATION	CHARGING_COST	PROPORTION_OF_DAILY_CHARGING_DURATION	COST_VS_AVG
+Chicago						2024-01-01			2.6					14.86			0.0048175							-8.85632
+Chicago						2024-01-01			2.0					21.31			0.0037058							-2.40632
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+SELECT user_id,
+       charging_station_id,
+       energy_consumed,
+       energy_consumed - AVG(energy_consumed) OVER(PARTITION BY charging_station_id) AS consumed_least
+FROM COURSE_41767_DB_0EC63EFF65AD43B293A6EA6C08D1759E.ELECTRIC_VEHICLES.CHARGING
+ORDER BY charging_station_id, consumed_least, user_id;
+
+USER_ID,CHARGING_STATION_ID,ENERGY_CONSUMED,CONSUMED_LEAST
+User_89,Station_1,33,-18.000
+User_1172,Station_1,45,-6.000
+User_794,Station_1,75,24.000
+User_1027,Station_275,1,-46.333
+User_233,Station_275,69,21.667
+User_589,Station_275,72,24.667
+User_331,Station_276,15,-17.500
+User_1035,Station_276,50,17.500
+User_1250,Station_277,14,-21.666
+User_984,Station_277,27,-8.666
+User_997,Station_277,66,30.334
+User_139,Station_278,74,-1.500
+User_350,Station_278,77,1.500
+User_535,Station_279,19,-35.666
+User_591,Station_279,45,-9.666
+User_324,Station_279,53,-1.666
+User_39,Station_279,62,7.334
+User_1084,Station_279,69,14.334
+User_1162,Station_279,80,25.334
+User_660,Station_40,,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+-- Running calculation
+-- ADD inside ur WINDOW FN ()
+-- AFTER 'PARTITION BY ... ORDER BY ...'
+-- ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+-- vs
+-- ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
+
+SELECT
+	user_id,
+  charging_station_location,
+	TO_DATE(charging_start_time),
+  charging_duration,
+  -- Find the running average of charging duration
+  AVG(charging_duration) OVER(
+      -- Partition the results by charging_station_location
+      PARTITION BY charging_station_location
+
+      -- Sequence the results by charging start time in ascending order
+      ORDER BY charging_start_time
+
+      -- Create the window of records to always be between the 
+      -- first row and the current row
+      ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+
+  ) AS running_average
+FROM ELECTRIC_VEHICLES.charging;
+
+USER_ID	CHARGING_STATION_LOCATION	TO_DATE(CHARGING_START_TIME)	CHARGING_DURATION	RUNNING_AVERAGE
+User_10	Chicago	2024-01-01	2.0	2.0000
+User_18	Chicago	2024-01-01	0.8	1.4000
+User_23	Chicago	2024-01-01	2.6	1.8000
+User_30	Chicago	2024-01-02	3.2	2.1500
+User_32	Chicago	2024-01-02	2.0	2.1200
+
+
+
+
+
+
+
+SELECT
+    user_id,
+    TO_DATE(charging_start_time), 
+    charging_station_location,
+    charging_rate,
+	
+    -- Find the average charging rate, by charging station location
+  	-- using a window frame between the first row and current row
+    AVG(charging_rate) OVER(
+        PARTITION BY charging_station_location
+        ORDER BY charging_start_time
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS running_average_charging_rate,
+	
+    -- Count the number of records by charging station location
+    COUNT(*) OVER(
+        PARTITION BY charging_station_location
+      	
+      	-- Create a window frame between the current row and the 
+      	-- last row, ordered by charging start time
+        ORDER BY charging_start_time
+        ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
+      
+    ) AS remaining_charges
+
+FROM ELECTRIC_VEHICLES.charging
+ORDER BY charging_station_location, charging_start_time;
+
+USER_ID	TO_DATE(CHARGING_START_TIME)	CHARGING_STATION_LOCATION	CHARGING_RATE	RUNNING_AVERAGE_CHARGING_RATE	REMAINING_CHARGES
+User_10	2024-01-01	Chicago	12	12.000	242
+User_18	2024-01-01	Chicago	7	9.500	241
+User_23	2024-01-01	Chicago	19	12.666	240
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SLIDING WINDOW
+-- MOVING AVERAGE, MOVING TOTAL
+-- ROWS BETWEEN X PRECEDING AND Y FOLLOWING
+-- X = N_ROWS B4
+-- Y = N_ROWS AFTER
+
+-- Moving vs. running
+YES - A running calculation TYPICALLY has a FIXED FIRST record in a window frame specified using UNBOUNDED PRECEDING.
+VS
+YES - A MOVING calculation does NOT have a FIXED STARTING OR ENDING RECORD in a window frame, whereas a running calculation does.
+YES - Seeing ROWS BETWEEN 2 PRECEDING AND 1 FOLLOWING is indicative of a MOVING calculation.
+NO - CURRENT ROW cannot be used in the OVER clause of a window function when performing a moving calculation.
+
+	
+
+SELECT
+    vehicle_model,
+    charger_type,
+    temperature,
+    charging_rate,
+
+	-- DISCREPANCY BETWEEN
+    -- WRITTEN INSTRUCTIONS
+    -- Find the moving average of temperature using the "two preceding records and the current row", ordered by temperature.
+    -- VS
+    -- AUTOGRADER AND CODE INSTRUCTIONS
+    -- Create a moving average of temperature using the two preceding and following records
+    AVG(temperature) OVER(
+        PARTITION BY vehicle_model
+        ORDER BY temperature
+        -- ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+        ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING
+    ) AS moving_average_temperature,
+    
+    -- Find the moving average charging rate for the preceding four charging sessions
+    AVG(charging_rate) OVER(
+        PARTITION BY vehicle_model
+        ORDER BY temperature
+        ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+    ) AS moving_average_charging_rate
+
+FROM ELECTRIC_VEHICLES.charging
+
+-- Only include non-NULL charging rates
+-- IN THE SQL, THE ROWS `WHERE charging_rate IS NOT NULL` IS FILTERED AND KEPT, THEN AGGREGATIONS AND WINDOW FNS ARE DONE?
+-- YES
+WHERE charging_rate IS NOT NULL
+ORDER BY vehicle_model, charger_type, temperature;
+
+VEHICLE_MODEL	CHARGER_TYPE	TEMPERATURE	CHARGING_RATE	MOVING_AVERAGE_TEMPERATURE	MOVING_AVERAGE_CHARGING_RATE
+BMW i3	DC Fast Charger	-11	46	-10.333	46.000
+BMW i3	DC Fast Charger	-10	5	-9.600	24.250
+BMW i3	DC Fast Charger	-8	25	-8.400	24.400
+BMW i3	DC Fast Charger	-8	7	-7.600	26.800
+BMW i3	DC Fast Charger	-6	32	-6.000	21.000
+BMW i3	DC Fast Charger	-6	30	-5.600	20.200
+BMW i3	DC Fast Charger	-5	29	-4.800	31.400
+BMW i3	DC Fast Charger	-4	34	-4.000	26.200
+BMW i3	DC Fast Charger	-3	40	-3.000	29.800
+BMW i3	DC Fast Charger	-3	11	-3.000	25.200
+BMW i3	DC Fast Charger	-2	38	-1.800	26.800
+BMW i3	DC Fast Charger	-1	19	-1.400	23.000
+BMW i3	DC Fast Charger	-1	34	-1.200	26.000
+BMW i3	DC Fast Charger	0	38	-0.200	20.400
+
+
+
+
+
+
+
+
+
+
+	
+SELECT
+    charging_station_location,
+    TO_DATE(charging_start_time),
+    charging_cost,
+    energy_consumed,
+	
+    -- Provide a ranking for each charging session based on energy consumed, from greatest to least
+    RANK() OVER(
+        PARTITION BY charging_station_location
+        ORDER BY energy_consumed DESC
+    ) AS rank_energy_consumed,
+	
+    -- Generate a "running total" of charging costs by charging station location
+    SUM(charging_cost) OVER(
+        PARTITION BY charging_station_location
+        ORDER BY charging_start_time
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS running_total_charging_cost,
+	
+    -- Build a window frame using the two preceding and two following sessions to find a moving average of energy consumed
+    AVG(energy_consumed) OVER(
+        PARTITION BY charging_station_location
+        ORDER BY charging_start_time
+        ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING
+    ) AS moving_average_energy_consumed
+
+FROM ELECTRIC_VEHICLES.charging
+WHERE energy_consumed IS NOT NULL
+ORDER BY charging_station_location, charging_start_time;
+
+CHARGING_STATION_LOCATION	TO_DATE(CHARGING_START_TIME)	CHARGING_COST	ENERGY_CONSUMED	RANK_ENERGY_CONSUMED	RUNNING_TOTAL_CHARGING_COST	MOVING_AVERAGE_ENERGY_CONSUMED
+Chicago	2024-01-01	21.31	78.869	8	21.31	50.629666
+Chicago	2024-01-01	6.43	23.824	180	27.74	52.120500
+Chicago	2024-01-01	14.86	49.196	108	42.60	49.354200
+Chicago	2024-01-02	33.13	56.593	76	75.73	41.568600
+Chicago	2024-01-02	17.01	38.289	135	92.74	42.946000
+Chicago	2024-01-02	15.86	39.941	130	108.60	46.304000
+Chicago	2024-01-02	18.13	30.711	156	126.73	39.364000
+
+
+
 
 
 
